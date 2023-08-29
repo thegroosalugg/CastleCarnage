8feae3ca70af320964bad3a12c0447ccfc5f5404
Victor Loginov <tkoe88@zoho.com>
2023-08-29 12:36:29 +0200

    progress

--- game/escape_room.rb
+++ game/escape_room.rb
@@ -2,24 +2,46 @@
 #-----------------------------YOUR CODE BELOW---------------------------------->
 
 ROOM_NAMES = [
-  { name: "Glowing Light Room", probability: (1..10).to_a },
-  { name: "Musty Library", probability: (1..10).to_a },
-  { name: "Treasure Chamber", probability: (1..10).to_a },
-  { name: "Dark Cellar", probability: (1..10).to_a },
-  { name: "Jacuzzi Room", probability: (1..10).to_a },
-  { name: "Dracula's Royce", probability: (1..10).to_a },
-  { name: "Dining Room", probability: (1..10).to_a },
-  { name: "The Roof", probability: (1..10).to_a },
-  { name: "The Lounge", probability: (1..10).to_a },
-  { name: "Secret door behind the painting of the old woman", probability: (1..10).to_a },
-  { name: "Trapdoor under the carpet", probability: (1..10).to_a },
-  { name: "Through the window", probability: (1..10).to_a }
+  { name: "Glowing Light Room", probability: (1..19).to_a },
+  { name: "Musty Library", probability: (1..19).to_a },
+  { name: "Treasure Chamber", probability: (1..19).to_a },
+  { name: "Dark Cellar", probability: (1..19).to_a },
+  { name: "Jacuzzi Room", probability: (1..19).to_a },
+  { name: "Dracula's Royce", probability: (1..19).to_a },
+  { name: "Dining Room", probability: (1..19).to_a },
+  { name: "The Roof", probability: (1..19).to_a },
+  { name: "The Lounge", probability: (1..19).to_a },
+  { name: "Secret door behind the painting of the old woman", probability: (1..19).to_a },
+  { name: "Trapdoor under the carpet", probability: (1..19).to_a },
+  { name: "Through the window", probability: (1..19).to_a }
 ]
 
 def inside_room(selected_room, enemy, weapon, player, second_enemy)
-  puts selected_room
-  # case selected_room[:probability]
-  # when
+  puts "You bolted off to the #{selected_room[:name]}"
+  puts "#{selected_room[:probability]}"
+  randomizer = rand(5..25)
+  case selected_room[:probability]
+  when 1..3
+    player[:hp] += randomizer
+    puts "Holy shit! Some food! Better eat up. You gained #{randomizer} HP!"
+  when 4..6
+    enemy[:hp] += randomizer
+    puts "Blyat! Shouldn't have some here. Enemy gained #{randomizer} HP!"
+  when 6..9
+    player[:hp] -= randomizer
+    puts "Bollocks, you stepped into a bear trap, this is gonna cost ya! You lost #{randomizer} HP!"
+  when 10..12
+    enemy[:hp] -= randomizer
+    puts "Booya, running after you the #{enemy[:name]} ate a bomb!. #{enemy[:name]} lost #{randomizer} HP"
+  when 13..17
+    weapon = pick_weapon
+    puts "Gift! There's a #{weapon[:name]} here! Looks like the foot's on the other shoe!"
+  when 18
+    second_enemy = random_enemy
+    puts "Oh fuck! You just dun goofed, #{second_enemy[:name]} jumped out at you!"
+  when 19
+    puts "You searched the room but found nothing."
+  end
 end
 
 def explore_rooms(enemy, weapon, player, second_enemy)
