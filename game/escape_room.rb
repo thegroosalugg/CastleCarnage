e1185a0195cea297e10a18409e117a6e9ea701dc
Victor Loginov <tkoe88@zoho.com>
2023-08-29 19:10:18 +0200

    Fully supports 2 enemies ongoing

--- game/escape_room.rb
+++ game/escape_room.rb
@@ -2,18 +2,18 @@
 #-----------------------------YOUR CODE BELOW---------------------------------->
 
 ROOM_NAMES = [
-  { name: "Glowing Light Room", probability: (1..19).to_a },
-  { name: "Musty Library", probability: (1..19).to_a },
-  { name: "Treasure Chamber", probability: (1..19).to_a },
-  { name: "Dark Cellar", probability: (1..19).to_a },
-  { name: "Jacuzzi Room", probability: (1..19).to_a },
-  { name: "Dracula's Royce", probability: (1..19).to_a },
-  { name: "Dining Room", probability: (1..19).to_a },
-  { name: "The Roof", probability: (1..19).to_a },
-  { name: "The Lounge", probability: (1..19).to_a },
-  { name: "Secret door behind the painting of the old woman", probability: (1..19).to_a },
-  { name: "Trapdoor under the carpet", probability: (1..19).to_a },
-  { name: "Through the window", probability: (1..19).to_a }
+  { name: "Glowing Light Room 💡", probability: (1..9).to_a },
+  { name: "Musty Library 📙", probability: (1..9).to_a },
+  { name: "Treasure Chamber 🏴‍☠️", probability: (1..9).to_a },
+  { name: "Dark Cellar 😱", probability: (1..9).to_a },
+  { name: "Jacuzzi Room 🩲👙", probability: (1..9).to_a },
+  { name: "Dracula's Royce 🚓", probability: (1..9).to_a },
+  { name: "Dining Room 🍽", probability: (1..9).to_a },
+  { name: "The Roof 🏰🌔", probability: (1..9).to_a },
+  { name: "The Lounge 📺", probability: (1..9).to_a },
+  { name: "Secret door behind the painting of the old woman 🖼", probability: (1..9).to_a },
+  { name: "Trapdoor under the carpet 🕳", probability: (1..9).to_a },
+  { name: "Through the window 🔲", probability: (1..9).to_a }
 ]
 
 def inside_room(selected_room, enemy, weapon, player, second_enemy)
@@ -21,32 +21,35 @@ def inside_room(selected_room, enemy, weapon, player, second_enemy)
   puts "#{selected_room[:probability]}"
   randomizer = rand(5..25)
   case selected_room[:probability]
-  when 1..3
-    player[:hp] += randomizer
-    puts "Holy shit! Some food! Better eat up. You gained #{randomizer} HP!"
-  when 4..6
-    enemy[:hp] += randomizer
-    puts "Blyat! Shouldn't have some here. Enemy gained #{randomizer} HP!"
-  when 6..9
-    player[:hp] -= randomizer
-    puts "Bollocks, you stepped into a bear trap, this is gonna cost ya! You lost #{randomizer} HP!"
-  when 10..12
-    enemy[:hp] -= randomizer
-    puts "Booya, running after you the #{enemy[:name]} ate a bomb!. #{enemy[:name]} lost #{randomizer} HP"
-  when 13..17
+  # when 1
+  #   player[:hp] += randomizer
+  #   puts "Holy shit! Some food! Better eat up. You gained #{randomizer} HP!"
+  # when 2
+  #   enemy[:hp] += randomizer
+  #   puts "Blyat! Shouldn't have some here. Enemy gained #{randomizer} HP!"
+  # when 3
+  #   player[:hp] -= randomizer
+  #   puts "Bollocks, you stepped into a bear trap, this is gonna cost ya! You lost #{randomizer} HP!"
+  # when 4
+  #   enemy[:hp] -= randomizer
+  #   puts "Booya, running after you the #{enemy[:name]} ate a bomb!. #{enemy[:name]} lost #{randomizer} HP"
+  when 1..4
     weapon = pick_weapon
     puts "Gift! There's a #{weapon[:name]} here! Looks like the foot's on the other shoe!"
-  when 18
+  when 5..8
     if second_enemy.nil?
       second_enemy = random_enemy
       puts "Oh fuck! You just dun goofed, #{second_enemy[:name]} jumped out at you!"
+    elsif enemy.nil?
+      enemy = random_enemy
+      puts "Motherfucker, #{enemy[:name]} sprung out the fridge, he coming right for ya!"
     else
       puts "You have an ominous feeling someone was just here"
     end
-  when 19
+  when 9
     puts "You searched the room but found nothing."
   end
-  return weapon, second_enemy
+  return enemy, weapon, second_enemy
 end
 
 def explore_rooms(enemy, weapon, player, second_enemy)
@@ -57,7 +60,12 @@ def explore_rooms(enemy, weapon, player, second_enemy)
     puts "[#{i + 1}] (DEBUG: #{room[:probability]}) #{room[:name]}"
   end
 
-  puts "Fuck, gotta pick a gaff quick, or I'm #{enemy[:name]} food!!"
+  if enemy
+    puts "Fuck, gotta pick a gaff quick, or I'm #{enemy[:name]} food!!"
+  elsif second_enemy
+    puts "Fuck, gotta pick a gaff quick, or I'm #{second_enemy[:name]} food!!"
+  end
+
   user_choice = gets.chomp.to_i
 
   until [1, 2, 3].include?(user_choice)
--- game/interface.rb
+++ game/interface.rb
@@ -7,7 +7,7 @@ require_relative 'escape_room'
 
 print `clear`
 
-player = { hp: rand(50..99), block: (1..10).to_a }
+player = { hp: rand(500..990), block: (1..10).to_a }
 enemy = random_enemy
 second_enemy = nil
 weapon = pick_weapon
@@ -18,9 +18,9 @@ intro_weapon(weapon, enemy)
 puts "#{enemy[:name]} HP: #{enemy[:hp].to_i}"
 puts "Your 💖 HP: #{player[:hp].to_i}"
 
-while enemy[:hp] > 0 && player[:hp] > 0
+while (enemy != nil && second_enemy != nil) || player[:hp] > 0
   puts "Whatcha ya gonna do?"
-  puts "[t]: T is for time to die fucker! [r]: Try a sommersault! [y]: Fuckin' leg it!"
+  puts "[t]⚔: T is for time to die fucker! [r]🤸‍♂️: Try a sommersault! [y]🏃‍♂️: Fuckin' leg it!"
   user_action = gets.chomp.downcase
 
   if user_action == "t"
@@ -32,27 +32,45 @@ while enemy[:hp] > 0 && player[:hp] > 0
       player_attack(second_enemy, weapon)
       enemy_attack(second_enemy, player)
     end
+
   elsif user_action == "r"
     print `clear`
-    somersault_attack(enemy, weapon, player)
+
+  elsif user_action == "r"
+    print `clear`
+
+    if enemy && second_enemy
+      random_enemy = [enemy, second_enemy].sample
+      somersault_attack(random_enemy, weapon, player)
+    elsif enemy
+      somersault_attack(enemy, weapon, player)
+    elsif second_enemy
+      somersault_attack(second_enemy, weapon, player)
+    end
+
 
   elsif user_action == "y"
     print `clear`
-    weapon, second_enemy = explore_rooms(enemy, weapon, player, second_enemy)
+    enemy, weapon, second_enemy = explore_rooms(enemy, weapon, player, second_enemy)
 
   else
     print `clear`
     puts "Don't be a fool"
   end
 
-  if second_enemy.nil?
-    puts "#{enemy[:name]} HP: #{enemy[:hp].to_i}"
-    puts "Your 💖 HP: #{player[:hp].to_i}"
-  else
-    puts "#{enemy[:name]} HP: #{enemy[:hp].to_i}"
-    puts "#{second_enemy[:name]} HP: #{second_enemy[:hp].to_i}"
-    puts "Your 💖 HP: #{player[:hp].to_i}"
+  if enemy && enemy[:hp] <= 0
+    enemy = nil
   end
+
+  if second_enemy && second_enemy[:hp] <= 0
+    second_enemy = nil
+  end
+
+
+  puts "#{enemy[:name]} HP: #{enemy[:hp].to_i}" if enemy
+  puts "#{second_enemy[:name]} HP: #{second_enemy[:hp].to_i}" if second_enemy
+  puts "Your 💖 HP: #{player[:hp].to_i}"
+
 end
 
-enemy[:hp] <= 0 ? win_message(enemy) : lose_message(enemy)
+enemy == nil && second_enemy == nil ? win_message(enemy) : lose_message(enemy)
