c5a87d6171671a3bf331c7461c371c54ceadfdee
victor loginov <tkoe88@zoho.com>
2023-09-24 15:39:30 +0200

    Improved health display. Minimum 1 heart always displays

--- CastleCarnage/boss/messages/boss_menu.rb
+++ CastleCarnage/boss/messages/boss_menu.rb
@@ -10,7 +10,7 @@ end
 
 def game_info(player, the_boss, boss_style, load_boss)
   puts "----------------------------------------------------------------------"
-  puts "    🧀 The Big Cheese HP: #{the_boss[:hp].to_i} #{'💜' * [the_boss[:hp] / 50, 0].max}"
+  puts "    🧀 The Big Cheese HP: #{the_boss[:hp].to_i} #{'💜' * [(the_boss[:hp] - 1) / 50 + 1, 0].max}"
   puts "                💫 Style: #{boss_style}"
   puts "----------------------------------------------------------------------"
   puts "    #{player_bars(player)}"
