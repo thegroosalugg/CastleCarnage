690d94792ec6fdd715413ace76496f003a1394e1
victor loginov <tkoe88@zoho.com>
2023-09-24 18:17:41 +0200

    refactored method

--- CastleCarnage/boss/boss_battle.rb
+++ CastleCarnage/boss/boss_battle.rb
@@ -1,6 +1,16 @@
 # rubocop:disable all
 #-----------------------------YOUR CODE BELOW---------------------------------->
 
+def pay_the_tab(player, the_boss, damage)
+  cash_spent = player[:cash].zero? ? 0 : (1..[player[:cash], 6].min).to_a.sample
+  multiplier = cash_spent.zero? ? 0.6 : 1.0 + (cash_spent * 0.5)
+  total_damage = (damage * multiplier).to_i
+  the_boss[:hp] -= total_damage
+  player[:cash] -= cash_spent
+  player[:drunk] = [player[:drunk] + cash_spent, 20].min
+  total_damage
+end
+
 def fight_the_power(player, the_boss, boss_style, load_boss)
 
   user_choice = 0
@@ -13,12 +23,7 @@ def fight_the_power(player, the_boss, boss_style, load_boss)
     user_choice = gets.chomp.to_i
 
     if user_choice == 4
-      cash_spent = player[:cash].zero? ? 0 : (1..[player[:cash], 6].min).to_a.sample
-      multiplier = cash_spent.zero? ? 0.6 : 1.0 + (cash_spent * 0.5)
-      total_damage = (damage * multiplier).to_i
-      the_boss[:hp] -= total_damage
-      player[:cash] -= cash_spent
-      player[:drunk] = [player[:drunk] + cash_spent, 20].min
+      total_damage = pay_the_tab(player, the_boss, damage) # Get total damage
     elsif user_choice == 5
       puts "tba"
     else
