7c0f5b4397f37d50dd96ecdd21f7f1e7390d07e7
victor loginov <tkoe88@zoho.com>
2023-09-24 23:39:31 +0200

    dynamic player status for cash and drunkness

--- CastleCarnage/boss/messages/boss_menu.rb
+++ CastleCarnage/boss/messages/boss_menu.rb
@@ -8,14 +8,38 @@ def boss_menu
   puts "[5] 🩸🪄 Blood Magic"
 end
 
+def player_status(player)
+  wallet =
+    case player[:cash]
+    when 0..2   then "[    Skint af     🫥]"
+    when 4..6   then "[   Pocket Money  🤔]"
+    when 7..9   then "[ Got some Moolah 😐]"
+    when 10..13 then "[     Sorted      🫠]"
+    when 14..17 then "[   Filthy Rich   🤑]"
+    when 18..20 then "[  Totally Minted 😈]"
+    end
+
+  drunk =
+    case player[:drunk]
+    when 0..2   then "[Sober as a Judge ⚖️]"
+    when 4..6   then "[  Got a Buzz on  😉]"
+    when 7..9   then "[  Feeling Tipsy  😏]"
+    when 10..13 then "[ Out on the Razz 🥴]"
+    when 14..17 then "[  Proper Pissed  🤤]"
+    when 18..20 then "[  Fucking Wasted 😵]"
+    end
+
+  puts "    💰 #{wallet} [DEBUG #{player[:cash]}] #{'💵' * [player[:cash], 0].max}"
+  puts "    🍻 #{drunk} [DEBUG #{player[:drunk]}] #{'🍺' * [player[:drunk], 0].max}"
+end
+
 def game_info(player, the_boss, boss_style, load_boss)
   puts "----------------------------------------------------------------------"
   puts "    #{the_boss[:name]} HP: [#{the_boss[:hp].to_i}] #{'💜' * [(the_boss[:hp] - 1) / 50 + 1, 0].max}"
   puts "                💫 Style: #{boss_style}"
   puts "----------------------------------------------------------------------"
   puts "    #{player_bars(player)}"
-  puts "    💰  Wallet: [DEBUG #{player[:cash]}] #{'💵' * [player[:cash], 0].max}"
-  puts "    🥴🍾 Drunk: [DEBUG #{player[:drunk]}] #{'🍺' * [player[:drunk], 0].max}"
+  puts "    #{player_status(player)}"
   puts "----------------------------------------------------------------------"
   puts load_boss
 end
