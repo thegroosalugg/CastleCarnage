5d52ac55c60b2020b2c10f111679088292ebb715
victor loginov <tkoe88@zoho.com>
2023-09-26 00:00:20 +0200

    enemy, boss weapon and boss generator established

--- CastleCarnage/main/enemies_weapons.rb
+++ CastleCarnage/main/enemies_weapons.rb
@@ -90,23 +90,19 @@ def random_enemy
   enemy = {
     name: ENEMY_NAMES.sample,
     hp: rand(200..400),
-    attack: (20..60).to_a,
-    block: (1..20).to_a,
-    accuracy: (1..20).to_a,
-    crit_ch: (1..20).to_a,
-    crit_x: -> { rand(1.0..5.0) }
+    attack: (rand(20..30)..rand(31..60)).to_a,
+    block: (rand(1..5)..rand(6..15)).to_a,
+    accuracy: (1..rand(5..11)).to_a,
+    crit_ch: (1..rand(5..11)).to_a,
+    crit_x: -> { rand(rand(1.5..2.0)..rand(2.0..4.0)) }
   }
 end
 
 def big_boss_awaits
   boss = {
     name: BOSS_NAMES.sample,
-    hp: rand(200..400),
-    attack: (20..60).to_a,
-    block: (1..20).to_a,
-    accuracy: (1..20).to_a,
-    crit_ch: (1..20).to_a,
-    crit_x: -> { rand(1.0..5.0) },
+    hp: rand(600..1000),
+    attack: (rand(20..50)..rand(51..90)).to_a,
     style: ["🍻 Barkeep", "🕶️ Bouncer"]
   }
 end
@@ -115,20 +111,20 @@ def pick_weapon
   weapon = {
     name: WEAPON_NAMES.sample,
     durability: rand(1..5),
-    attack: (20..60).to_a,
-    accuracy: (1..20).to_a,
-    crit_ch: (1..20).to_a,
-    crit_x: -> { rand(1.0..5.0) }
+    attack: (rand(20..30)..rand(31..60)).to_a,
+    accuracy: (1..rand(5..11)).to_a,
+    crit_ch: (1..rand(5..11)).to_a,
+    crit_x: -> { rand(rand(1.5..2.5)..rand(2.6..5.0)) }
   }
 end
 
 def special_weapon
   special_weapon = {
     name: SPECIAL_WEAPON_NAMES.sample,
-    durability: rand(1..3),
-    attack: (20..60).to_a,
-    accuracy: (1..20).to_a,
-    crit_ch: (1..20).to_a,
-    crit_x: -> { rand(1.0..5.0) }
+    durability: rand(1..2),
+    attack: (rand(30..40)..rand(41..100)).to_a,
+    accuracy: (1..rand(6..12)).to_a,
+    crit_ch: (1..rand(6..12)).to_a,
+    crit_x: -> { rand(rand(2.0..3.0)..rand(3.1..5.0)) }
   }
 end
