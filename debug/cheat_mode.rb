e2c90413ad6dfb754089c5233906791a624bb387
victor loginov <tkoe88@zoho.com>
2023-10-01 23:15:55 +0200

    update cheat menu

--- debug/cheat_mode.rb
+++ debug/cheat_mode.rb
@@ -8,10 +8,10 @@ def cheat_menu(player, enemy, second_enemy, weapon, user_choice)
   case user_choice
   when "h"
     print `clear`
-    player[:hp] += 100
+    player[:hp] -= 100
   when "hh"
     print `clear`
-    player[:hp] -= 100
+    player[:hp] += 100
   when "w"
     print `clear`
     weapon = pick_weapon
@@ -24,16 +24,16 @@ def cheat_menu(player, enemy, second_enemy, weapon, user_choice)
   when "w2"
     print `clear`
     weapon[:durability] += 1
-  when "e"
-    print `clear`
-    enemy = random_enemy
   when "ee"
     print `clear`
+    enemy = random_enemy
+  when "e"
+    print `clear`
     enemy[:hp] -= 100 if enemy
-  when "s"
+  when "ss"
     print `clear`
     second_enemy = random_enemy
-  when "ss"
+  when "s"
     print `clear`
     second_enemy[:hp] -= 100 if second_enemy
   end
@@ -45,12 +45,12 @@ end
 def cheat_menu_boss(user_choice, player, weapon, the_boss, boss_style)
 
   case user_choice
-  when "b"
-    print `clear`
-    the_boss[:hp] -= 200
   when "h"
     print `clear`
     player[:hp] -= 100
+  when "hh"
+    print `clear`
+    player[:hp] += 100
   when "w"
     print `clear`
     weapon = pick_weapon
@@ -63,6 +63,9 @@ def cheat_menu_boss(user_choice, player, weapon, the_boss, boss_style)
   when "w2"
     print `clear`
     weapon[:durability] += 1
+  when "b"
+    print `clear`
+    the_boss[:hp] -= 200
   when "1"
     print `clear`
     boss_style = "🍻 Barkeep"
@@ -72,13 +75,13 @@ def cheat_menu_boss(user_choice, player, weapon, the_boss, boss_style)
   when "3"
     print `clear`
     boss_style = "🚾 Toilet Guy"
-  when "x"
+  when "v"
     print `clear`
     player[:cash] -= 1
   when "c"
     print `clear`
     player[:cash] += 1
-  when "s"
+  when "f"
     print `clear`
     player[:drunk] -= 1
   when "d"
