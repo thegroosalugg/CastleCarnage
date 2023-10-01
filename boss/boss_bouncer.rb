48810b1dc8b6c2852ff2621ce47a296497451eaa
victor loginov <tkoe88@zoho.com>
2023-10-01 18:28:10 +0200

    created skeleton bouncer fight method

--- boss/boss_bouncer.rb
+++ boss/boss_bouncer.rb
@@ -3,7 +3,36 @@
 
 def fight_the_bouncer(player, weapon, the_boss, boss_style, load_boss)
 
-  puts "everything is fine with the bouncer"
+  user_choice = 0
+
+  until (4..7).include?(user_choice)
+    game_info(player, weapon, the_boss, boss_style, load_boss)
+    fight_menu(player, boss_style, weapon)
+
+    unarmed_damage = (player[:attack].sample * (100 - player[:drunk] * 5) / 100).to_i
+    weapon_damage = ((player[:attack].sample + weapon[:attack].sample) * (100 - player[:drunk] * 5) / 100).to_i
+    user_choice = gets.chomp.to_i
+
+    puts unarmed_damage
+    puts weapon_damage
+
+    case user_choice
+    when 4
+      print `clear`
+      #total_damage = pay_the_tab(player, the_boss, damage) # Get total damage
+    when 5
+      print `clear`
+      #total_damage = bar_fight(player, the_boss)
+    when 6
+      print `clear`
+    when 7
+      print `clear`
+    else
+      error_message
+    end
+  end
+
+  #damage_info(the_boss, total_damage)
   boss_style = the_boss[:style].sample
   return boss_style, weapon
 end
