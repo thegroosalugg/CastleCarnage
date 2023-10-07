8eae3af80e77a5585ee945766a2c642600dcf81d
victor loginov <tkoe88@zoho.com>
2023-10-07 17:20:02 +0200

    connected main game to boss encounter

--- interface.rb
+++ interface.rb
@@ -83,10 +83,8 @@ def play_game
 
     tracked_enemy = enemies.sample if player[:hp] <= 0 # Player dies and last enemy is tracked
 
-    # ((enemies_defeated > 1) || (rooms_explored > 8) || (enemies_defeated > 0 && rooms_explored > 5)) && (rand(1..2) == 1) ? big_boss_battle : big_boss_warning
-    if rooms_explored.zero?
-      enemies_defeated = 2 # debug
-      rooms_explored = 5 # debug
+    if ((enemies_defeated > rand(3..5)) || (rooms_explored > rand(10..15)) || (enemies_defeated > rand(2..3) && rooms_explored > rand(5..10)))
+      enemies = []
       tracked_enemy = the_boss
       bonus(player, rooms_explored, enemies_defeated)
       big_boss_battle(player, weapon, the_boss)
