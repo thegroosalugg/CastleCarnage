0c2096732430fe9878631ab92de57b51f667d078
victor loginov <tkoe88@zoho.com>
2023-10-09 16:32:05 +0200

    minor renaming

--- boss/powers/blackjack.rb
+++ boss/powers/blackjack.rb
@@ -3,20 +3,20 @@
 
 def deck
   deck = []
-  suits = ["♠️", "♥️", "♦️", "♣️"] # Define an array of emojis representing card faces
-  royals = { "Ķ" => 10, "ʠ" => 10, "J" => 10, "Ā" => 11 } # Define face cards and their values
+  suits = ["♠️", "♥️", "♦️", "♣️"] # Define an array of emojis representing card suits
+  royals = { "K" => 10, "Q" => 10, "J" => 10, "A" => 11 } # Define suit cards and their values
   values = (2..10).to_a # Define card values for numbered cards (2 to 10)
 
-  suits.each do |face| # Add numbered cards to the deck
+  suits.each do |suit| # Add numbered cards to the deck
     values.each do |value|
-      card = { face: "#{face} #{value}", value: value }
+      card = { suit: "#{suit} #{value}", value: value }
       deck << card
     end
   end
 
-  suits.each do |face| # Add face cards to the deck
+  suits.each do |suit| # Add suit cards to the deck
     royals.each do |name, value|
-      card = { face: "#{face} #{name}", value: value }
+      card = { suit: "#{suit} #{name}", value: value }
       deck << card
     end
   end
@@ -32,7 +32,7 @@ def blackjack(player, the_boss)
   boss_hand << deck.shift(2)
   your_hand << deck.shift(2)
 
-  boss_cards, your_cards = [boss_hand, your_hand].map { |hand| hand.flatten.map { |card| card[:face] } }
+  boss_cards, your_cards = [boss_hand, your_hand].map { |hand| hand.flatten.map { |card| card[:suit] } }
   boss_total, your_total = [boss_hand, your_hand].map { |hand| hand.flatten.sum { |card| card[:value] } }
 
   puts "Boss: #{boss_cards.join(', ')} (Total: #{boss_total})"
