f2fbeca7f1ba7e1a8ce21484901241bd62a8ba1f
victor loginov <tkoe88@zoho.com>
2023-10-09 13:26:19 +0200

    draw hands

--- boss/powers/blackjack.rb
+++ boss/powers/blackjack.rb
@@ -4,7 +4,7 @@
 def deck
   deck = []
   suits = ["♠️", "♥️", "♦️", "♣️"] # Define an array of emojis representing card faces
-  royals = { "Ķ" => 10, "ʠ" => 10, "J" => 10, "Ā" => 11 } # Define face cards and their values (e.g., King, Queen, Jack)
+  royals = { "Ķ" => 10, "ʠ" => 10, "J" => 10, "Ā" => 11 } # Define face cards and their values
   values = (2..10).to_a # Define card values for numbered cards (2 to 10)
 
   suits.each do |face| # Add numbered cards to the deck
@@ -25,5 +25,16 @@ def deck
 end
 
 def blackjack(player, the_boss)
-  puts deck
+  deck
+  boss_hand = []
+  your_hand = []
+
+  boss_hand << deck.shift(2)
+  your_hand << deck.shift(2)
+
+  boss_cards, your_cards = [boss_hand, your_hand].map { |hand| hand.flatten.map { |card| card[:face] } }
+  boss_total, your_total = [boss_hand, your_hand].map { |hand| hand.flatten.sum { |card| card[:value] } }
+
+  puts "Boss: #{boss_cards.join(', ')} (Total: #{boss_total})"
+  puts " You: #{your_cards.join(', ')} (Total: #{your_total})"
 end
