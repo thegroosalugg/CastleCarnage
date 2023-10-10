a6388bb5a46d47e1112c4063d05706329d442411
victor loginov <tkoe88@zoho.com>
2023-10-10 22:46:02 +0200

    move some stuff around

--- boss/combat/messages/war_letters.rb
+++ boss/combat/messages/war_letters.rb
@@ -1,36 +1,6 @@
 # rubocop:disable all
 #-----------------------------YOUR CODE BELOW---------------------------------->
 
-# Intro outro messages for changes to boss fighting style
-
-def boss_walks(the_boss, boss_style, time)
-  barkeep = [
-    "The #{boss_style} is in the house 🛖 orders up",
-    "The #{boss_style} is keeping the liquor flowng, its happy hour ⌚",
-  ]
-  bouncer = [
-    "#{the_boss[:name]} is excerting its pressure, you drink another beer 🍺",
-  ]
-  band = [
-    "#{the_boss[:name]} is playing 🎸 rocking music, a mosh pit is about to break out!",
-    "#{the_boss[:name]} is two-stepping across the room 🤾, Parkway Drive 🎸 appear on stage!",
-    "Some wanker just started playing dubstep 🎧 you're gonna take some serious damage this round.",
-  ]
-  outro = [
-    "#{the_boss[:name]} assumes the #{boss_style} fighting stance, watch out!",
-  ]
-  reprise = [
-    "That was quite a mash up, you're one 🍺 lighter.",
-  ]
-
-  messages = case boss_style
-  when "🍻 Barkeep" then time == :intro ? barkeep : outro
-  when "🕶️ Bouncer" then time == :intro ? bouncer : outro
-  when "🎶 Band"    then (time == :reprise) ? reprise : (time == :intro) ? band : outro
-  end
-  puts text_break(messages.sample, " ", 70)
-end
-
 # Invoice for stat changes
 # Bar fight outcomes # .abs removes negatives so only positive integers displayed
 
@@ -121,14 +91,6 @@ def step_on_up(method)
     puts "[4] 🃏 Hit me!"
     puts "[5] 🪂 I'm out!"
   end
-end
-
-def greeting
-  messages = [
-    "Step on up and show who's the boss",
-  ]
-
-  puts text_break(messages.sample, " ", 70)
 end
 
 def show_your_moves(player, the_boss, user_moves, boss_moves, method)
--- boss/messages/talks_with_the_boss.rb
+++ boss/messages/talks_with_the_boss.rb
@@ -12,3 +12,41 @@ def your_rewards(player, health, boost, multiplier)
 
   puts text_break(messages.sample, " ", 70)
 end
+
+def greeting
+  messages = [
+    "Step on up and show who's the boss",
+  ]
+
+  puts text_break(messages.sample, " ", 70)
+end
+
+# Intro outro messages for changes to boss fighting style
+
+def boss_walks(the_boss, boss_style, time)
+  barkeep = [
+    "The #{boss_style} is in the house 🛖 orders up",
+    "The #{boss_style} is keeping the liquor flowng, its happy hour ⌚",
+  ]
+  bouncer = [
+    "#{the_boss[:name]} is excerting its pressure, you drink another beer 🍺",
+  ]
+  band = [
+    "#{the_boss[:name]} is playing 🎸 rocking music, a mosh pit is about to break out!",
+    "#{the_boss[:name]} is two-stepping across the room 🤾, Parkway Drive 🎸 appear on stage!",
+    "Some wanker just started playing dubstep 🎧 you're gonna take some serious damage this round.",
+  ]
+  outro = [
+    "#{the_boss[:name]} assumes the #{boss_style} fighting stance, watch out!",
+  ]
+  reprise = [
+    "That was quite a mash up, you're one 🍺 lighter.",
+  ]
+
+  messages = case boss_style
+  when "🍻 Barkeep" then time == :intro ? barkeep : outro
+  when "🕶️ Bouncer" then time == :intro ? bouncer : outro
+  when "🎶 Band"    then (time == :reprise) ? reprise : (time == :intro) ? band : outro
+  end
+  puts text_break(messages.sample, " ", 70)
+end
