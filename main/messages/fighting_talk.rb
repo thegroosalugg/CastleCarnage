8e516aae2984cd3e528774326625c4df0bb1d26d
victor loginov <tkoe88@zoho.com>
2023-10-14 03:32:07 +0200

    refctored fighting talk messages

--- main/messages/fighting_talk.rb
+++ main/messages/fighting_talk.rb
@@ -5,7 +5,7 @@
 
 def shots_fired(attacker, target, damage = 0, outcome)
   critical = "#{attacker[:name]} ⚔️ \e[38;5;208m𝙲𝚁𝙸𝚃𝙸𝙲𝙰𝙻\e[0m 💥 #{target[:name]} -#{damage} #{target[:emoji]}"
-  missed = "\e[34m𝘔𝘐𝘚𝘚\e[0m #{attacker[:name]} 💭❓"
+  missed = "#{attacker[:name]} 💭❓ \e[34m𝘔𝘐𝘚𝘚\e[0m"
   on_point = "#{attacker[:name]} ⚔️ \e[31m𝙷𝙸𝚃\e[0m 💢 #{target[:name]} -#{damage} #{target[:emoji]}"
   counter = "#{attacker[:name]} 🗯️❗ ⚔️ \e[36mℂ𝕆𝕌ℕ𝕋𝔼ℝ\e[0m 💢 #{target[:name]} -#{damage} #{target[:emoji]}"
 
@@ -21,24 +21,9 @@ end
 # Sommersault attack
 
 def somersault(chance, n)
-  success = [
-    "You turned Super Saiyan 😼, the scouter 🥽 says your power level is over 9000!",
-    "⬇ ↘ ➡ HP LP ⬅ ↖ ⬆ LP ⬆ ↖ ⬅ HK  ⬇ LK HP  ⬇ ↘ ➡ ➡ ⬆ ⬇ HK ⟪⟪Ultra Combo⟫⟫",
-    "With outcomes like these 💯 you might win after all, roll 🎲 #{n} times!",
-    "You ran up the wall and did a sick backflip, you strike #{n} times!",
-    "The gods ⚖ smile upon you, your path is righteous",
-    "Bang on ⚖. You get #{n} extra attacks!",
-  ]
-  failed = [
-    "You shouldn't have eaten #{n} pizzas 🍕 before trying to do acrobatics 🤢.",
-    "You tripped over your own shoelaces 👞 and knocked yourself out 😵.",
-    "You're still pretty wasted 🍻, you can't even stand straight.",
-    "It ain't gonna happen today mate 🚷, take bare damage bruv",
-    "You stacked 😵 it! They gone have a field day, you plum!",
-    "You thought you had it, but you got merked instead 😭",
-    "The enemy laughed 😂 and socked 👊 you in the face.",
-  ]
+  success = "\e[32m𝓢𝓤𝓒𝓒𝓔𝓢𝓢\e[0m " + "⚔️ " * n
+  failed = "\e[31m𝓢𝓣𝓐𝓒𝓚𝓔𝓓 𝓘𝓣\e[0m " + "😓 " * n
 
   messages = chance == 1 ? success : failed
-  puts text_break(messages.sample, " ", 80)
+  puts text_break(messages, " ", 80)
 end
