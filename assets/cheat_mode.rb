db0c469b0fd99fcf4ab645839acea22a85134a1e
victor loginov <tkoe88@zoho.com>
2023-10-17 15:36:00 +0200

    rerouted interface choice

--- assets/cheat_mode.rb
+++ assets/cheat_mode.rb
@@ -3,8 +3,8 @@
 
 # debug cheat menu for interface
 
-def cheat_menu(player, enemies, choice)
-  case choice
+def cheat_menu(player, enemies)
+  case player[:choice]
   when 'e' then enemies.first[:hp] -= 100
   when 's' then enemies << random_enemy
   when "h" then player[:hp] -= 100
@@ -25,33 +25,4 @@ def cheat_menu(player, enemies, choice)
   player[:drunk] = player[:drunk].clamp(0, 5)
   player[:cash] = player[:cash].clamp(0, 5)
   return enemies
-end
-
-# boss menu cheat interface. Not possible to combine the 2
-
-def cheat_menu_boss(choice, player, buddy, weapon, the_boss, boss_style)
-  case choice
-  when "h"  then player[:hp] -= 100
-  when "hh" then player[:hp] += 100
-  when "w1" then weapon = pick_weapon
-  when "w2" then weapon = special_weapon
-  when "w"  then weapon[:durability] += 1
-  when "ww" then weapon[:durability] -= 1
-  when "b"  then the_boss[:hp] -= 200
-  when "a"  then the_boss[:rage] += 1
-  when "e"  then buddy[:hp] -= 100 if buddy
-  when "ee" then buddy = random_enemy
-  when "1"  then boss_style = :barkeep
-  when "2"  then boss_style = :bouncer
-  when "3"  then boss_style = :band
-  when "v"  then player[:cash] -= 1
-  when "c"  then player[:cash] += 1
-  when "f"  then player[:drunk] -= 1
-  when "d"  then player[:drunk] += 1
-  end
-  print `clear`
-  weapon[:durability] = weapon[:durability].clamp(0, 10)
-  player[:cash] = player[:cash].clamp(0, 20)
-  player[:drunk] = player[:drunk].clamp(0, 20)
-  return buddy, weapon, boss_style
 end
