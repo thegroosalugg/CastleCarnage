908679968686496e9885c49a9845cf9169233579
victor loginov <tkoe88@zoho.com>
2023-10-18 01:49:09 +0200

    refactored weapon generator. no more repetition

--- main/war_machine.rb
+++ main/war_machine.rb
@@ -31,65 +31,6 @@ def random_enemy
   }
 end
 
-# Method to pick a weapon
-def pick_weapon
-  regular = ["#{YL}#{WEAPONS.sample}", 2, 5, 1, 5, -5, -0.3]
-  special = ["#{MG}#{SPECIAL.sample}", 3, 10, 4, 7, -7, 0.0]
-  name, us, at, bk, ac, ch, x = rand(4) == 1 ? special : regular
-
-  weapon = {
-    name: "#{name}#{CL}",
-    uses: rand(us..5),
-    attack: rand(at..15),
-    block: rand(bk..8),
-    accuracy: rand(-3..ac),
-    crit_ch: rand(ch..1),
-    crit_x: rand(x..1.5)
-  }
-end
-
-def equip_weapon(wielder)
-  if wielder[:equipped]
-    wielder[:uses] = 0
-    weapon_breaks(wielder)
-  end
-  preset = [:attack, :block, :accuracy, :crit_ch, :crit_x]
-  boost = [:weapon_attack, :weapon_block, :weapon_accuracy, :weapon_crit_ch, :weapon_crit_x]
-
-  weapon = pick_weapon
-  wielder[:equipped] = weapon[:name]
-  wielder[:uses] = weapon[:uses]
-
-  preset.each_with_index do |stat, i1|
-    boost.each_with_index do |boost, i2|
-      if i1 == i2
-        wielder[boost] = weapon[stat]
-        wielder[stat] += weapon[stat]
-      end
-    end
-  end
-  weapon_speaks(wielder, wielder[:equipped], (wielder[:id] == :player ? :got : :enemy))
-end
-
-def weapon_breaks(wielder)
-  if wielder[:uses] == 0
-    weapon_speaks(wielder, wielder[:equipped], :broke) if wielder[:id] == :player
-
-    preset = [:attack, :block, :accuracy, :crit_ch, :crit_x]
-    boost = [:weapon_attack, :weapon_block, :weapon_accuracy, :weapon_crit_ch, :weapon_crit_x]
-
-    preset.each_with_index do |stat, i1|
-      boost.each_with_index do |boost, i2|
-        if i1 == i2
-          wielder[stat] -= wielder[boost]
-          wielder[boost] = 0
-        end
-      end
-    end
-    wielder[:equipped] = nil
-  end
-end
-
 def room_vault
   chosen_rooms = []
 
@@ -107,4 +48,61 @@ def room_vault
                                           # ANSI color hard coded here or all rooms get same color
   chosen_rooms.map { |room| room[:name] = "\e[3#{rand(1..6)}m#{DESC.sample} #{room[:name]}#{CL}" } # Give the rooms a unique desc
   chosen_rooms
+end
+
+# Method to create a weapon
+def pick_weapon
+  regular = ["#{YL}#{WEAPONS.sample}", 2, 5, 1, 5, -5, -0.3]
+  special = ["#{MG}#{SPECIAL.sample}", 3, 10, 4, 7, -7, 0.0]
+  name, us, at, bk, ac, ch, x = rand(4) == 1 ? special : regular
+
+  weapon = {
+    name: "#{name}#{CL}",
+    uses: rand(us..5),
+    attack: rand(at..15),
+    block: rand(bk..8),
+    accuracy: rand(-3..ac),
+    crit_ch: rand(ch..1),
+    crit_x: rand(x..1.5)
+  }
+end
+
+def adjust_stats(wielder, request, weapon = nil) # takes player/enemy & saves weapon stats as new keys
+  preset = [:attack, :block, :accuracy, :crit_ch, :crit_x] # based on request saved stats are added or removed and reset
+  boost = [:weapon_attack, :weapon_block, :weapon_accuracy, :weapon_crit_ch, :weapon_crit_x]
+
+  preset.each_with_index do |stat, i1|
+    boost.each_with_index do |boost, i2|
+      if i1 == i2
+        if request == :add
+          wielder[boost] = weapon[stat]
+          wielder[stat] += wielder[boost]
+        else
+          wielder[stat] -= wielder[boost]
+          wielder[boost] = 0
+        end
+      end
+    end
+  end
+end
+
+def equip_weapon(wielder)
+  if wielder[:equipped] # first thing check for and destroy existing weapon
+    wielder[:uses] = 0
+    weapon_breaks(wielder)
+  end
+  weapon = pick_weapon # create weapon and assign to player
+  wielder[:equipped] = weapon[:name]
+  wielder[:uses] = weapon[:uses]
+
+  adjust_stats(wielder, :add, weapon) # saves weapon stats as new keys and adds them to player stats
+  weapon_speaks(wielder, wielder[:equipped], (wielder[:id] == :player ? :got : :enemy)) # decides whose message plays
+end
+
+def weapon_breaks(wielder)
+  if wielder[:uses] == 0
+    weapon_speaks(wielder, wielder[:equipped], :broke) if wielder[:id] == :player # don't care about enemy weapons breaking
+    adjust_stats(wielder, :remove) # no weapon required to remove weapon stats, we use newly created keys to change values back
+    wielder[:equipped] = nil # delete weapon
+  end
 end
