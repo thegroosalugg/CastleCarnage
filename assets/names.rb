0a87567fe3f5fd48241ff8682364db2552a98020
victor loginov <tkoe88@zoho.com>
2023-10-23 04:56:02 +0200

    cleaned up repetitive rooms names

--- assets/names.rb
+++ assets/names.rb
@@ -14,33 +14,32 @@ ENEMIES = [
 ]
 WEAPONS = [
   "🏏 Fungo Bat", "🥊 Bx Gloves", "🔪 Machete", "🔱 Spear", "🗡️ Sword", "🎇 Mace", "🍢 Club", "🔧 Wrench", "🪤 Trap", "🦯 Staff",
-  "🪄 Scepter", "🏹 Crossbow", "🪚 Chainsaw", "🔫 MAC-11", "🪃 Boomerang", "⚔️ Battle Axe", "🪓 Hatchet", "🧨 Dynamite",
+  "🪄 Scepter", "🏹 Crossbow", "🪚 Chainsaw", "🔫 MAC-11", "🪃 Boomerang", "⚔️ Battle Ax", "🪓 Hatchet", "🧨 Dynamite",
 ]
 SPECIAL = [
   "🪡 Laser Gun", "🚀 RPG", "🔥 Fireball", "💣 Bo-Bomb", "🛫 AirStrike","🦄 Unicorn"
 ]
 ROOMS = [
   "Bathroom 🛁", "Dining Room 🍽️", "Library 📚", "Bedroom 🛌", "Kitchen 🍳", "Living Room 🛋",
-  "Game Room 🎮", "Home Office 🖥", "Garage 🚗", "Attic 🕸", "Basement 🕳", "Garden 🌳",
-  "Patio 🏞", "Balcony 🏙", "Pantry 🍏", "Closet 🧥", "Laundry Room 🧺", "Guest Bedroom 🛌",
-  "Study 📖", "Foyer 🚪", "Sunroom ☀️", "Mudroom 🧦", "Theater Room 🎬", "Gym for Muscles 🏋️‍♂️",
-  "Pool Room 🎱", "Wine Cellar 🍷", "Music Room 🎵", "Art Studio 🎨", "Craft Room ✂️", "Nightclub 🕋",
-  "Workshop ⚒️", "Sauna 🧖‍♂️", "Guest Bathroom 🚿", "Playroom 🧸", "Nursery 👶", "Bar 🍸",
-  "Cinema 🍿", "Billiards Room 🎱", "Den 📺", "Lounge 🍹", "Conservatory 🌿",
-  "Porch 🏡", "Walk-In Closet 👗", "Wine Tasting Room 🍇", "Cigar Lounge 🚬", "Reading Nook 📚",
-  "Secret Passage 🗝", "Observatory 🔭", "Greenhouse 🌱", "Dance Studio 💃", "Recording Studio 🎤",
-  "Arcade 🕹️", "Arcane Library 📜", "Chapel ⛪", "Crypt 💀", "Hidden Chamber 🕳", "Laboratory 🧪",
+  "Attic 🕸", "Basement 🕳", "Garden 🌳", "Patio 🏞", "Balcony 🏙", "Pantry 🍏", "Gym for Muscles 🏋️‍♂️",
+  "Nightclub 🕋", "Workshop ⚒️", "Sauna 🧖‍♂️", "Lounge 🍹", "Conservatory 🌿", "Porch 🏡", "Secret Passage 🗝",
+  "Observatory 🔭", "Greenhouse 🌱","Chapel ⛪", "Crypt 💀", "Chamber 🕳", "Laboratory 🧪",
 ]
 DESC = [
   "💩 The Manky", "⛏️ The Shoddy", "🧛 Dracula's", "🏢 The Swanky", "🐝 The Buzzing", "🍷 The Dodgy", "🩸 The Bloody",
-  "🎉 The Reem", "🐴 The Pony", "🤬 The Bastard", "🤩 The Peng", "🤪 The Mental", "👹 The Wicked", "🎭 The Dramatic",
-  "🎤 The Groovy", "🎃 The Spooky", "🚢 The Nautical", "🍔 The Greasy", "🌑 The Abandoned", "😵 The Bonkers", "🎸 The Rocking",
-  "🍄 The Shroomy", "🦁 The Roaring", "🌵 The Deserted", "🤢 The Gnarly", "🏛️ The Presidential", "🔮 The Trippy", "🤮 The Minging",
-  "🐒 The Cheeky", "🗣️ The Banter", "🅱️ The Bitchin'", "👻 The Haunted", "🐺 The Howling",
+  "🤬 The Bastard", "🤩 The Peng", "🤪 The Mental", "👹 The Wicked", "🐺 The Howling", "😵 The Bonkers", "🤮 The Minging",
+  "🚢 The Nautical", "🍔 The Greasy", "🌑 The Abandoned", "🌵 The Deserted", "🤢 The Gnarly", "🏛️ The Presidential",
+  "🐒 The Cheeky", "🗣️ The Banter", "🅱️ The Bitchin'", "👻 The Haunted",
 ]
 ITEMS = [
   "🍋 Lemons", "🥛 Old Milk", "🥬 Kale", "🍔 Mackie D's", "🍟 Cold Chips",
   "🍕 Floor Pizza", "🍩 Stale Donut", "🎂 Fucking Cake", "🍌 Mouldy Bananas",
   "☕ Instant Coffee", "🥞 Burnt Pancakes", "🌿 Northern Lights", "🌭 Tinned Hot Dogs",
   "🔷 Heisenberg's Blue", "🥟 Questionnable Taco", "🌶️ Really Spicy Pepper", "🍗 Undercooked Chicken"
+]
+COLUMN_A = [
+  "🥏🪁🛢️⚱️💫☄️🌟♨️🪔",
+]
+COLUMN_B = [
+  "🥏🗑️🪟🪞🚽🛏️🛋️🛁🚿🪁🛢️⚱️💫☄️🌟♨️🪔",
 ]
