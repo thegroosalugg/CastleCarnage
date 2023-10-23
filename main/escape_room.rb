bc2b2e0df8d2d5323089303acfd2cb696adf6fbc
victor loginov <tkoe88@zoho.com>
2023-10-23 15:24:02 +0200

    all running correctly & refactored

--- main/escape_room.rb
+++ main/escape_room.rb
@@ -2,32 +2,18 @@
 #-----------------------------YOUR CODE BELOW---------------------------------->
 
 def escape_room(enemies, player)
-  choice          = 0
-  n               = (6 - player[:drunk]).clamp(1, 4) # fewer rooms available if player drunk
-  player[:rooms]  = room_vault(n) # creates n room
-  player[:land]   = { id: :room, art: ROOM_SERVICE.sample } # sets the scene
+  run_away(player)
 
-  shout(player, :escape)
-  game_info(enemies, player)
-
-  until (4..(3 + player[:rooms].length)).include?(choice) # 4..(3 + rooms length are set to the indexes are 4-7 instead of 0-3
+  until (4..(3 + player[:rooms].length)).include?(player[:choice]) # 4..(3 + rooms length are set to the indexes are 4-7 instead of 0-3
+    game_info(enemies, player)
     puts MENU_HEADER
     player[:rooms].each_with_index { |room, i| puts " " * 22 + "#{ML}#{NUM[i + 4]}#{CL} #{room[:name]}" } # +4 to index
     puts BARRIER
-    choice = gets.chomp.to_i
-
-    shout(player, :error)      # unless (4..7).include?(choice)
-    game_info(enemies, player) # unless (4..7).include?(choice)
+    player[:choice] = gets.chomp.to_i
+    shout(player, :error)
   end
-
   print `clear`
-  player[:room]   = player[:rooms][choice - 4] # -4 to correct index
-  #p player[:rooms]
-  player[:scout] += 1 # updates visited counter
-  player[:drunk].times { player[:room][:chance] << 3 } # 1 extra chance to meet an enemy per level of drunk
-  player[:roll]   = player[:room][:chance].sample # final outcome is then sampled
-  p player[:room] # DEBUG
-  shout(player, :room)
+  open_door(player)
 
   case player[:roll]
   when 1 then crap_factory(player)
@@ -36,4 +22,20 @@ def escape_room(enemies, player)
   end
   target          = [player, enemies.sample].sample
   surprise(enemies, player)
+end
+
+def run_away(player)
+  player[:choice] = 0
+               n  = (6 - player[:drunk]).clamp(1, 4) # fewer rooms available if player drunk
+  player[:rooms]  = room_vault(n) # creates n room
+  player[:land]   = { id: :room, art: ROOM_SERVICE.sample } # sets the scene
+  shout(player, :escape)
+end
+
+def open_door(player)
+  player[:room]   = player[:rooms][player[:choice] - 4] # -4 to correct index
+  player[:scout] += 1 # updates visited counter
+  player[:drunk].times { player[:room][:chance] << 3 } # 1 extra chance to meet an enemy per level of drunk
+  player[:roll]   = player[:room][:chance].sample # final outcome is then sampled
+  shout(player, :room)
 end
